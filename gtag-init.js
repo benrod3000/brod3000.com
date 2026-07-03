6053020c96dcd97b3fceed952b8f8da154521664
Ben <131934797+benrod3000@users.noreply.github.com>
2026-07-03 12:58:56 -0700

    Update gtag-init.js

--- gtag-init.js
+++ gtag-init.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-X42B535184');
+gtag('config', 'G-X42B535184'); 
